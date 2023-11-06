d43f9718f8e2df61f8b41aabf53682ba2ee6e75f
Rayhan Alghiffari Azizi <rayhan.azizi9@gmail.com>
2023-11-06 23:26:26 +0700

    gd_extension_util: improve performance by allocating array size

--- extension-src/gd_extension_utils.cpp
+++ extension-src/gd_extension_utils.cpp
@@ -5,11 +5,13 @@ std::string from_godot(const godot::String &s) { return s.utf8().get_data(); }
 
 godot::TypedArray<godot::String> to_godot(const std::vector<std::string> &v) {
     godot::TypedArray<godot::String> result;
-    for (const auto &s : v) result.push_back(to_godot(s));
+    result.resize(v.size());
+    for (int i = 0; i < v.size(); i++) result[i] = to_godot(v[i]);
     return result;
 }
 std::vector<std::string> from_godot(const godot::TypedArray<godot::String> &v) {
     std::vector<std::string> result;
-    for (int i = 0; i < v.size(); i++) result.push_back(from_godot((godot::String)v[i]));
+    result.resize(v.size());
+    for (int i = 0; i < v.size(); i++) result[i] = from_godot((godot::String)v[i]);
     return result;
 }
