b11c5b308985dcd32a01f82ee637b4fe36667c92
Rayhan Alghiffari Azizi <rayhan.azizi9@gmail.com>
2023-11-06 14:51:39 +0700

    equaio

---
+++ equaio/Equaio.gd
@@ -0,0 +1,13 @@
+extends GDEquaio
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	var x = self.factorial(5);
+	print(x);
+	pass
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
